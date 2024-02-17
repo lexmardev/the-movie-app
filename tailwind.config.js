419849577004573acb7362f34d53a94a8154f05c
Alexander Leiva <lexmardev@gmail.com>
2024-02-17 10:25:39 -0600

    feat: add icons and animations plugins for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const {
+  iconsPlugin,
+  getIconCollections,
+} = require("@egoist/tailwindcss-icons");
+import animations from "@midudev/tailwind-animations";
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    animations,
+    iconsPlugin({
+      collections: getIconCollections(["tabler"]),
+    }),
+  ],
 };
