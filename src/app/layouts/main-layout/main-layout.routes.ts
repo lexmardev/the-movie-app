dbceebae3e60134f69afb73a9013af3ab1b48cde
Alexander Leiva <lexmardev@gmail.com>
2024-02-16 23:12:41 -0600

    feat: add main layout

---
+++ src/app/layouts/main-layout/main-layout.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+
+export const MainLayoutRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full',
+  },
+  {
+    path: 'movies',
+    loadComponent: () =>
+      import('../../modules/movie/presentation/movie.component').then(
+        (m) => m.MovieComponent
+      ),
+    loadChildren: () =>
+      import('../../modules/movie/presentation/movie.routes').then(
+        (m) => m.MovieRoutes
+      ),
+  },
+];
