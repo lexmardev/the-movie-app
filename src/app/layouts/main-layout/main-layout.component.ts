9cda16b7110af622890a72fb45b1101f0484b9fa
Alexander Leiva <lexmardev@gmail.com>
2024-02-17 15:24:30 -0600

    feat: add header in main layout

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './components/header.component'
 
 @Component({
-  selector: 'app-main-layout',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+	selector: 'app-main-layout',
+	standalone: true,
+	imports: [RouterOutlet, HeaderComponent],
+	template: `
+		<div class="flex flex-col">
+			<app-header />
+			<div class="container flex">
+				<router-outlet />
+			</div>
+		</div>
+	`,
 })
 export class MainLayoutComponent {}
