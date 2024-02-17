45b0b0f5f18b3068be3d719ae9b22a8e4ad6718c
Alexander Leiva <lexmardev@gmail.com>
2024-02-17 17:07:54 -0600

    feat: add aspect ratio tailwind's plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const {
-  iconsPlugin,
-  getIconCollections,
-} = require("@egoist/tailwindcss-icons");
-import animations from "@midudev/tailwind-animations";
+const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons')
+import animations from '@midudev/tailwind-animations'
 
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("@tailwindcss/forms"),
-    animations,
-    iconsPlugin({
-      collections: getIconCollections(["tabler"]),
-    }),
-  ],
-};
+	content: ['./src/**/*.{html,ts}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/aspect-ratio'),
+		animations,
+		iconsPlugin({
+			collections: getIconCollections(['tabler']),
+		}),
+	],
+}
