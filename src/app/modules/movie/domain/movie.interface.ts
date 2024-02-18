910e7daafec107d7b24bc3f6308d4df599c0007d
Alexander Leiva <lexmardev@gmail.com>
2024-02-17 18:03:14 -0600

    feat: is favorite property added

--- src/app/modules/movie/domain/movie.interface.ts
+++ src/app/modules/movie/domain/movie.interface.ts
@@ -1,50 +1,51 @@
 export interface IMovie {
-  _id?: string;
-  id?: string;
-  primaryImage?: PrimaryImage;
-  titleType?: TitleType;
-  titleText?: TitleText;
-  originalTitleText?: TitleText;
-  releaseYear?: ReleaseYear;
-  releaseDate?: ReleaseDate;
+	_id?: string
+	id?: string
+	primaryImage?: PrimaryImage
+	titleType?: TitleType
+	titleText?: TitleText
+	originalTitleText?: TitleText
+	releaseYear?: ReleaseYear
+	releaseDate?: ReleaseDate
+	isFavorite?: boolean
 }
 
 interface TitleText {
-  text?: string;
-  __typename?: string;
+	text?: string
+	__typename?: string
 }
 
 interface PrimaryImage {
-  id?: string;
-  width?: number;
-  height?: number;
-  url?: string;
-  caption?: Caption;
-  __typename?: string;
+	id?: string
+	width?: number
+	height?: number
+	url?: string
+	caption?: Caption
+	__typename?: string
 }
 
 interface Caption {
-  plainText?: string;
-  __typename?: string;
+	plainText?: string
+	__typename?: string
 }
 
 interface ReleaseDate {
-  day?: null;
-  month?: null;
-  year?: number;
-  __typename?: string;
+	day?: null
+	month?: null
+	year?: number
+	__typename?: string
 }
 
 interface ReleaseYear {
-  year?: number;
-  endYear?: null;
-  __typename?: string;
+	year?: number
+	endYear?: null
+	__typename?: string
 }
 
 interface TitleType {
-  text?: string;
-  id?: string;
-  isSeries?: boolean;
-  isEpisode?: boolean;
-  __typename?: string;
+	text?: string
+	id?: string
+	isSeries?: boolean
+	isEpisode?: boolean
+	__typename?: string
 }
