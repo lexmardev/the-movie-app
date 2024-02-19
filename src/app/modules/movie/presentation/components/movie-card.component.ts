0848b01a80c204b7d2e9106f3a8294cdbe45934f
Alexander Leiva <lexmardev@gmail.com>
2024-02-18 23:49:04 -0600

    feat: add edit icon

--- src/app/modules/movie/presentation/components/movie-card.component.ts
+++ src/app/modules/movie/presentation/components/movie-card.component.ts
@@ -1,5 +1,6 @@
 import { NgClass } from '@angular/common'
 import { Component, Input, OnInit } from '@angular/core'
+import { RouterLink } from '@angular/router'
 import { MovieImagePipe } from '@lib/pipes/movie-image.pipe'
 import { MovieService } from '@lib/services/movie.service'
 import { IMovie } from '@modules/movie/domain/movie.interface'
@@ -8,7 +9,7 @@ import { MovieControllerService } from '@modules/movie/infrastructure/movie.cont
 @Component({
 	selector: 'app-movie-card',
 	standalone: true,
-	imports: [NgClass, MovieImagePipe],
+	imports: [NgClass, MovieImagePipe, RouterLink],
 	template: `
 		<div class="group">
 			<div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg sm:aspect-h-3 sm:aspect-w-2">
@@ -21,9 +22,14 @@ import { MovieControllerService } from '@modules/movie/infrastructure/movie.cont
 					<div class="flex h-1/2 w-full flex-col justify-end bg-gradient-to-t from-black to-transparent">
 						<h1 class="w-full pb-4 text-center text-2xl font-bold text-white">{{ movie.titleText?.text }}</h1>
 						<div class="flex flex-row justify-between">
-							<button (click)="deleteMovie()" class="cursor-pointer pb-4 pl-4">
-								<i class="i-tabler-trash h-8 w-8 text-gray-400 hover:scale-110"></i>
-							</button>
+							<div>
+								<button (click)="deleteMovie()" class="cursor-pointer pb-4 pl-4">
+									<i class="i-tabler-trash h-8 w-8 text-gray-400 hover:scale-110"></i>
+								</button>
+								<a [routerLink]="['/movies/edit', movie.id]">
+									<i class="i-tabler-edit h-8 w-8 text-gray-400 hover:scale-110"></i>
+								</a>
+							</div>
 							<button (click)="toggleFavorite()" class="cursor-pointer pb-4 pr-4">
 								<i
 									class="i-tabler-heart h-8 w-8 text-gray-400"
